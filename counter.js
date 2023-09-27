a3f7c47a5327b0746ed1397751bee1b17e057a23
tinykite <dakota@tinykitelab.com>
2023-09-27 14:49:26 -0500

    feat: Add counter

---
+++ counter.js
@@ -0,0 +1,26 @@
+import { LitElement, html } from 'https://cdn.skypack.dev/lit-element'
+
+class Counter extends LitElement {
+
+    static properties = {
+        counter: { type: Number},
+    }
+
+    constructor () {
+        super()
+        this.counter = 0
+    }
+
+    increment() {
+        this.counter++
+    }
+
+    render() {
+        return html`
+        <div>${this.counter}</div>
+        <button @click="${this.increment}">Increment</button>
+        `
+    }
+}
+
+customElements.define('qty-picker', Counter)
